6b35015461a6b6554a1a44cb7bc14c40a77eb510
31702160217 <15815333425@163.com>
2019-04-09 16:44:45 +0800

    Initial commit

---
+++ app/src/main/java/com/jrz/mobliesafe1/chapter03/entity/ContactInfo.java
@@ -0,0 +1,11 @@
+package com.jrz.mobliesafe1.chapter03.entity;
+
+/**
+ * Created by Administrator on 2019/3/26.
+ */
+
+public class ContactInfo {
+    public String id;
+    public String name;
+    public String phone;
+}
---
+++ app/src/main/java/com/jrz/mobliesafe1/chapter03/reciever/InterceptSmsReciever.java
@@ -0,0 +1,45 @@
+package com.jrz.mobliesafe1.chapter03.reciever;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.telephony.SmsMessage;
+
+import com.jrz.mobliesafe1.chapter03.db.dao.BlackNumberDao;
+
+/**
+ * Created by Administrator on 2019/3/26.
+ */
+
+public class InterceptSmsReciever extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        SharedPreferences mSP = context.getSharedPreferences("config",
+                Context.MODE_PRIVATE);
+        boolean BlackNumStatus = mSP.getBoolean("BlackNumStatus", true);
+        if (!BlackNumStatus) {
+            // 黑名单拦截关闭
+            return;
+        }
+        // 如果是黑名单 则终止广播
+        BlackNumberDao dao = new BlackNumberDao(context);
+        Object[] objs = (Object[]) intent.getExtras().get("pdus");
+        for (Object obj : objs) {
+            SmsMessage smsMessage = SmsMessage.createFromPdu((byte[]) obj);
+            String sender = smsMessage.getOriginatingAddress();
+            String body = smsMessage.getMessageBody();
+            if (sender.startsWith("+86")) {
+                sender = sender.substring(3, sender.length());
+            }
+            int mode = dao.getBlackContactMode(sender);
+            if (mode == 2 || mode == 3) {
+                // 需要拦截短信，拦截广播
+                abortBroadcast();
+            }
+        }
+    }
+
+}
+
